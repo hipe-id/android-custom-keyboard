d71d1bf01c53c36ccf690754efb5deb061049d98
mochadwi <mochadwi20@gmail.com>
2019-07-28 15:27:56 +0700

    feat(sampleapp): Add keyboard as a service
    Rename the keyboard to use Hipe Keyboard
    
    todo: add icon

--- app/src/main/java/id/hipe/sampleapp/ui/MainFragment.kt
+++ app/src/main/java/id/hipe/sampleapp/ui/MainFragment.kt
@@ -1,8 +1,13 @@
 package id.hipe.sampleapp.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.View
-
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class MainFragment : Fragment() {
@@ -13,10 +18,40 @@ class MainFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         // TODO: Init or setup view, viewmodel related, etc anything to setup here
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // TODO: Init or setup view, viewmodel related, etc anything to setup here
+        startActivityForResult(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS), 99)
+
+        if (isInputEnabled()) {
+            (activity?.getSystemService(
+                Context.INPUT_METHOD_SERVICE) as InputMethodManager).showInputMethodPicker()
+        } else {
+            Toast.makeText(context, "Please enable keyboard first", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun isInputEnabled(): Boolean {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val mInputMethodProperties = imm.enabledInputMethodList
+
+        val N = mInputMethodProperties.size
+        var isInputEnabled = false
+
+        for (i in 0 until N) {
+
+            val imi = mInputMethodProperties[i]
+            Log.d("INPUT ID", imi.id.toString())
+            if (imi.id.contains(activity?.packageName ?: "")) {
+                isInputEnabled = true
+            }
+        }
+
+        return isInputEnabled
     }
 }
