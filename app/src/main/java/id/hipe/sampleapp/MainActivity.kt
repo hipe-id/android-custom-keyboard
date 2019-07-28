fac06db872812cfb8342fea13337d7e5207936b3
mochadwi <mochadwi20@gmail.com>
2019-07-28 15:58:38 +0700

    feat(sampleapp): Refine the sample app
    Using activity to redirect into keyboard settings, set state for text
    view if hipe keyboard currently enabled/disabled, add icon for the keyboard ime preference

--- app/src/main/java/id/hipe/sampleapp/MainActivity.kt
+++ app/src/main/java/id/hipe/sampleapp/MainActivity.kt
@@ -1,17 +1,61 @@
 package id.hipe.sampleapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import id.hipe.customkeyboard.R
-import id.hipe.sampleapp.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvKeyboard: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvKeyboard = findViewById(R.id.tvKeyboard)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.main_content, MainFragment())
+        tvKeyboard.text = "Hipe keyboard currently disabled"
+
+        startActivityForResult(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS), 99)
+
+        if (isInputEnabled()) {
+            (getSystemService(
+                Context.INPUT_METHOD_SERVICE) as InputMethodManager).showInputMethodPicker()
+        } else {
+            Toast.makeText(this@MainActivity, "Please enable keyboard first", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 99) {
+            tvKeyboard.text = "Hipe keyboard now enabled"
+        }
+    }
+
+    private fun isInputEnabled(): Boolean {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val mInputMethodProperties = imm.enabledInputMethodList
+
+        val N = mInputMethodProperties.size
+        var isInputEnabled = false
+
+        for (i in 0 until N) {
+
+            val imi = mInputMethodProperties[i]
+            Log.d("INPUT ID", imi.id.toString())
+            if (imi.id.contains(packageName ?: "")) {
+                isInputEnabled = true
+            }
+        }
+
+        return isInputEnabled
+    }
+
 }
